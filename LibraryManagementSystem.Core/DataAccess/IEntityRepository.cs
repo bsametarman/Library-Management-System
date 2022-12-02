495a718c42e7ee53b3c5e0ea530ae74e1b2320f7
bsametarman@outlook.com <bsametarman@outlook.com>
2022-12-02 16:45:08 +0300

    EntityRepository implementaiton

---
+++ LibraryManagementSystem.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T Get(Expression<Func<T, bool>> filter);
+    }
+}
