4228e1eda199c7b949199153ff7442cc3a3a5393
bsametarman@outlook.com <bsametarman@outlook.com>
2022-12-04 17:13:39 +0300

    Book service and manager added

---
+++ LibraryManagementSystem.Business/Abstract/IBookService.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Abstract
+{
+    public interface IBookService
+    {
+        List<Book> GetAll();
+        Book GetById(int id);
+        void Add(Book book);
+        void Update(Book book);
+        void Delete(Book book);
+
+    }
+}
---
+++ LibraryManagementSystem.Business/Concrete/BookManager.cs
@@ -0,0 +1,46 @@
+﻿using LibraryManagementSystem.Business.Abstract;
+using LibraryManagementSystem.DataAccess.Abstract;
+using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        private IBookDal _bookDal;
+
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+
+        public void Add(Book book)
+        {
+            _bookDal.Add(book);
+        }
+
+        public void Delete(Book book)
+        {
+            _bookDal.Delete(book);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _bookDal.GetAll();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookDal.Get(p => p.BookId == id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookDal.Update(book);
+        }
+    }
+}
