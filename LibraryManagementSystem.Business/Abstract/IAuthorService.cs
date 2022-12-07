0dfb027c25787ab39d85c355fd737ea7267077f7
bsametarman@outlook.com <bsametarman@outlook.com>
2022-12-07 15:06:47 +0300

    Business and DataAccess for Author entity

---
+++ LibraryManagementSystem.Business/Abstract/IAuthorService.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Abstract
+{
+    public interface IAuthorService
+    {
+        List<Author> GetAll();
+        Author GetById(int id);
+        void Add(Author author);
+        void Update(Author author);
+        void Delete(Author author);
+    }
+}
---
+++ LibraryManagementSystem.Business/Concrete/AuthorManager.cs
@@ -0,0 +1,46 @@
+﻿using LibraryManagementSystem.Business.Abstract;
+using LibraryManagementSystem.DataAccess.Abstract;
+using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Concrete
+{
+    public class AuthorManager : IAuthorService
+    {
+        private IAuthorDal _authorDal;
+
+        public AuthorManager(IAuthorDal authorDal)
+        {
+            _authorDal = authorDal;
+        }
+
+        public void Add(Author author)
+        {
+            _authorDal.Add(author);
+        }
+
+        public void Delete(Author author)
+        {
+            _authorDal.Delete(author);
+        }
+
+        public List<Author> GetAll()
+        {
+            return _authorDal.GetAll();
+        }
+
+        public Author GetById(int id)
+        {
+            return _authorDal.Get(a => a.AuthorId == id);
+        }
+
+        public void Update(Author author)
+        {
+            _authorDal.Update(author);
+        }
+    }
+}
