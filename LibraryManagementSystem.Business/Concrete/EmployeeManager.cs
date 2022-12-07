e154f060f05dee5f580bf357e2bb0656a5755687
bsametarman@outlook.com <bsametarman@outlook.com>
2022-12-07 16:45:44 +0300

    Employee methods added

---
+++ LibraryManagementSystem.Business/Concrete/EmployeeManager.cs
@@ -0,0 +1,46 @@
+﻿using LibraryManagementSystem.Business.Abstract;
+using LibraryManagementSystem.DataAccess.Abstract;
+using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Concrete
+{
+    public class EmployeeManager : IEmployeeService
+    {
+        private IEmployeeDal _employeeDal;
+
+        public EmployeeManager(IEmployeeDal employeeDal)
+        {
+            _employeeDal = employeeDal;
+        }
+
+        public void Add(Employee employee)
+        {
+            _employeeDal.Add(employee);
+        }
+
+        public void Delete(Employee employee)
+        {
+            _employeeDal.Delete(employee);
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _employeeDal.GetAll();
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employeeDal.Get(e => e.EmployeeId == id);
+        }
+
+        public void Update(Employee employee)
+        {
+            _employeeDal.Update(employee);
+        }
+    }
+}
---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagementSystem.Business.Abstract;
+using LibraryManagementSystem.Business.Concrete;
+using LibraryManagementSystem.DataAccess.Concrete;
+using LibraryManagementSystem.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class EmployeeController : ControllerBase
+    {
+        IEmployeeService employeeService = new EmployeeManager(new EfEmployeeDal());
+
+        [HttpGet]
+        public List<Employee> GetAll()
+        {
+            return employeeService.GetAll();
+        }
+    }
+}
