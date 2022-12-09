d5cd5ed120e4bdd2930223fbee71abc64eb7c218
bsametarman@outlook.com <bsametarman@outlook.com>
2022-12-09 19:44:58 +0300

    new classes and methods for genre entity

---
+++ LibraryManagementSystem.Business/Concrete/GenreManager.cs
@@ -0,0 +1,46 @@
+﻿using LibraryManagementSystem.Business.Abstract;
+using LibraryManagementSystem.DataAccess.Abstract;
+using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Concrete
+{
+    public class GenreManager : IGenreService
+    {
+        private IGenreDal _genreDal;
+
+        public GenreManager(IGenreDal genreDal)
+        {
+            _genreDal = genreDal;
+        }
+
+        public void Add(Genre genre)
+        {
+            _genreDal.Add(genre);
+        }
+
+        public void Delete(Genre genre)
+        {
+            _genreDal.Delete(genre);
+        }
+
+        public Genre Get(int id)
+        {
+            return _genreDal.Get(g => g.GenreId == id);
+        }
+
+        public List<Genre> GetAll()
+        {
+            return _genreDal.GetAll();
+        }
+
+        public void Update(Genre genre)
+        {
+            _genreDal.Update(genre);
+        }
+    }
+}
