ae851fa72f4624d88db6ad56d33c96c2dd1ddc3b
bsametarman@outlook.com <bsametarman@outlook.com>
2022-12-09 20:02:07 +0300

    New classes and methods for translator entity

---
+++ LibraryManagementSystem.Business/Abstract/ITranslatorService.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Abstract
+{
+    public interface ITranslatorService
+    {
+        List<Translator> GetAll();
+        Translator Get(int id);
+        void Add(Translator translator);
+        void Update(Translator translator);
+        void Delete(Translator translator);
+    }
+}
---
+++ LibraryManagementSystem.Business/Concrete/TranslatorManager.cs
@@ -0,0 +1,46 @@
+﻿using LibraryManagementSystem.Business.Abstract;
+using LibraryManagementSystem.DataAccess.Abstract;
+using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Concrete
+{
+    public class TranslatorManager : ITranslatorService
+    {
+        private ITranslatorDal _translatorDal;
+
+        public TranslatorManager(ITranslatorDal translatorDal)
+        {
+            _translatorDal = translatorDal;
+        }
+
+        public void Add(Translator translator)
+        {
+            _translatorDal.Add(translator);
+        }
+
+        public void Delete(Translator translator)
+        {
+            _translatorDal.Delete(translator);
+        }
+
+        public Translator Get(int id)
+        {
+            return _translatorDal.Get(t => t.TranslatorId == id);
+        }
+
+        public List<Translator> GetAll()
+        {
+            return _translatorDal.GetAll();
+        }
+
+        public void Update(Translator translator)
+        {
+            _translatorDal.Update(translator);
+        }
+    }
+}
