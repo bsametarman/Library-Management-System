21a8aada198ea6bd4ac4bfa06a3323069e6e7f9f
bsametarman@outlook.com <bsametarman@outlook.com>
2022-12-09 19:53:04 +0300

    New classes and methods for publisher entity

---
+++ LibraryManagementSystem.Business/Abstract/IPublisherService.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Abstract
+{
+    public interface IPublisherService
+    {
+        List<Publisher> GetAll();
+        Publisher Get(int id);
+        void Add(Publisher publisher);
+        void Update(Publisher publisher);
+        void Delete(Publisher publisher);
+
+    }
+}
---
+++ LibraryManagementSystem.Business/Concrete/PublisherManager.cs
@@ -0,0 +1,46 @@
+﻿using LibraryManagementSystem.Business.Abstract;
+using LibraryManagementSystem.DataAccess.Abstract;
+using LibraryManagementSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Business.Concrete
+{
+    public class PublisherManager : IPublisherService
+    {
+        private IPublisherDal _publisherDal;
+
+        public PublisherManager(IPublisherDal publisherDal)
+        {
+            _publisherDal = publisherDal;
+        }
+
+        public void Add(Publisher publisher)
+        {
+            _publisherDal.Add(publisher);
+        }
+
+        public void Delete(Publisher publisher)
+        {
+            _publisherDal.Delete(publisher);
+        }
+
+        public Publisher Get(int id)
+        {
+            return _publisherDal.Get(p => p.PublisherId == id);
+        }
+
+        public List<Publisher> GetAll()
+        {
+            return _publisherDal.GetAll();
+        }
+
+        public void Update(Publisher publisher)
+        {
+            _publisherDal.Update(publisher);
+        }
+    }
+}
