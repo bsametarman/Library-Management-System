25c32bee51809f7a52d0ef8d0e32c0f4c9c18aa7
bsametarman@outlook.com <bsametarman@outlook.com>
2023-02-12 17:31:58 +0300

    user password change added

---
+++ LibraryManagementSystem.MvcWebUI/Models/UserPasswordChangeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagementSystem.MvcWebUI.Models
+{
+    public class UserPasswordChangeViewModel
+    {
+        public string Id { get; set; }
+        public string Password { get; set; }
+    }
+}
